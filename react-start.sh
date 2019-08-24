3d9afae4fe7e6515fcec469db52be8032d7aabaf
grportes <sem_email@sem_email.com.br>
2019-08-24 15:22:34 -0300

    correções..

--- react-start.sh
+++ react-start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-baixar_dependencias() 
+#
+# function: baixar_dependencias
+#
+baixar_dependencias()
 {
 	echo "-------------------------------------------------------"
 	echo "Adicionando dependencias"
@@ -10,11 +13,11 @@ baixar_dependencias()
 	yarn add @material-ui/core 
 	yarn add @material-ui/icons
 	yarn add typeface-roboto
-	yarn add redux
-	yarn add react-redux
 	yarn add react-router
 	yarn add react-router-dom
 	yarn add classnames
+    yarn add typeface-roboto
+	yarn add prop-types
 
 	echo "-------------------------------------------------------"
 	echo "Adicionando dependencias p/ desenvolvimento"
@@ -32,10 +35,13 @@ baixar_dependencias()
 	yarn add -D @babel/preset-env
 	yarn add -D @babel/preset-react
 	yarn add -D @babel/plugin-proposal-class-properties
-	yarn add -D @babel/plugin-transform-class-properties
+	yarn add -D @babel/plugin-proposal-object-rest-spread
 
 }
 
+#
+# function: config_package_json
+#
 config_package_json()
 {
 	echo "-----------------------------------"
@@ -45,356 +51,426 @@ config_package_json()
 	mv newpackage.json package.json
 }
 
+#
+# function: criar_arquivo_babelrc()
+#
 criar_arquivo_babelrc()
 {
-	echo "---------------------------------"
+	echo "-------------------------------------------------------"
 	echo "CRIANDO .babelrc"
-	echo "---------------------------------"
-	babelrc='
-	{
-		"presets": [
-			"@babel/env",
-			"@babel/react"
-		],
-		"plugins": [
-			"@babel/plugin-proposal-class-properties"
-		]
-	}'
+	echo "-------------------------------------------------------"
+	babelrc='{
+	"presets": [
+		"@babel/env",
+		"@babel/react"
+	],
+	"plugins": [
+		"@babel/plugin-proposal-class-properties",
+		"@babel/plugin-proposal-object-rest-spread"
+	]
+}'
 	echo "$babelrc" > .babelrc
 }
 
+#
+# function: config_webpack()
+#
 config_webpack()
 {
 
-	echo "---------------------------------"
+	echo "-------------------------------------------------------"
 	echo "CRIANDO webpack.config.js"
-	echo "---------------------------------"
-	webpack="
-	const path = require('path');
-	const HtmlWebpackPlugin = require('html-webpack-plugin');
-	const CleanWebpackPlugin = require('clean-webpack-plugin');
+	echo "-------------------------------------------------------"
+	webpack="const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-	module.exports = {
+module.exports = {
 
-		mode: 'development',
+	mode: 'development',
 
-		output: {
-			path: path.resolve(__dirname, 'dist'),
-			filename: 'index_bundle.js',
-			publicPath: '/'
-		}, 
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'index_bundle_[contenthash].js',
+		publicPath: '/'
+	}, 
 
-		devtool: 'inline-source-map',
+	devtool: 'inline-source-map',
 
-		devServer: {
-			index: 'index.html',
-			contentBase: './dist',
-			historyApiFallback: true,
-		 	inline: true,
-			open: true,
-			port: 8080
-		},	
+	devServer: {
+		index: 'index.html',
+		contentBase: path.resolve(__dirname, 'dist'),
+		historyApiFallback: true,
+	 	inline: true,
+		open: true,
+		port: 8080
+	},	
 
-		plugins: [
-			new CleanWebpackPlugin(['dist']),
-			new HtmlWebpackPlugin({
-				title: 'Teste',
-				template: './src/index.html',
-				filename: './index.html'
-			})
-		],
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			title: 'Teste',
+			template: 'src/index.html',
+			filename: 'index.html'
+		})
+	],
 
-		module: {
-			rules: [
-				{
-					test: /\.css$/,
-					use: [ 'style-loader', 'css-loader' ]
-				},
-				{
-					test: /\.(png|svg|jpg|gif)$/,
-					use: [ 'file-loader' ]
-				},
-				{
-					test: /\.(woff|woff2|eot|ttf|otf)$/,
-					use: [ 'file-loader' ]
-				},
-				{
-					test: /\.js$/,
-					exclude: /node_modules/,
-					use: [ 'babel-loader']
-				}
-			]
-		}
-	};"
+	module: {
+		rules: [
+			{
+				test: /\.css$/,
+				use: [ 'style-loader', 'css-loader' ]
+			},
+			{
+				test: /\.(png|svg|jpg|gif)$/,
+				use: [ 'file-loader' ]
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				use: [ 'file-loader' ]
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: [ 'babel-loader']
+			}
+		]
+	}
+};"
 	echo "$webpack" > webpack.config.js
-
 }
 
-
+#
+# function: criar_folder_src()
+#
 criar_folder_src()
 {
-	echo "---------------------------------"
+	echo "-------------------------------------------------------"
 	echo "CRIANDO FOLDER src"
-	echo "---------------------------------"
+	echo "-------------------------------------------------------"
 	mkdir src
 
-	echo "---------------------------------"
+	echo "-------------------------------------------------------"
 	echo "CRIANDO src/index.html"
-	echo "---------------------------------"
-	tmp='
-	<!DOCTYPE html>
-	<html lang="pt-br">
-		<head>
-			<meta charset="UTF-8">
-		  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-			<meta http-equiv="X-UA-Compatible" content="ie=edge">
-			<title>TESTE</title>
-		</head>
-		<body>
-			<div id='app'></div>
-		</body>
-	</html>
-	'
+	echo "-------------------------------------------------------"
+	tmp='<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8">
+	  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>TESTE</title>
+	</head>
+	<body>
+		<div id='app'></div>
+	</body>
+</html>'
 	echo "$tmp" > src/index.html
 
-	echo "---------------------------------"
+	echo "-------------------------------------------------------"
 	echo "CRIANDO src/index.css"
-	echo "---------------------------------"
-	tmp="
-	body
-	{
-		 margin: 0;
-		 padding: 0;
-		 font-family: 'Roboto', sans-serif;
-		 background-color: #E4E4E4;
-	}
-	"
+	echo "-------------------------------------------------------"
+	tmp="body
+{
+	 margin: 0;
+	 padding: 0;
+	 font-family: 'Roboto', sans-serif;
+	 background-color: #E4E4E4;
+}"
 	echo "$tmp" > src/index.css
 
-	echo "---------------------------------"
+	echo "-------------------------------------------------------"
 	echo "CRIANDO src/index.js"
-	echo "---------------------------------"
-	tmp="
-	import React from 'react';
-	import ReactDOM from 'react-dom';
+	echo "-------------------------------------------------------"
+	tmp="import React from 'react';
+import ReactDOM from 'react-dom';
 
-	import App from './components/App';
+import App from './components/App';
 
-	ReactDOM.render(<App /> ,document.getElementById('app'));
-	"
+ReactDOM.render(<App /> ,document.getElementById('app'));"
 	echo "$tmp" > src/index.js
 
-	echo "---------------------------------"
+
+	echo "-------------------------------------------------------"
 	echo "CRIANDO FOLDER src/components"
-	echo "---------------------------------"
+	echo "-------------------------------------------------------"
 	mkdir src/components
 
-	echo "---------------------------------"
+
+	echo "-------------------------------------------------------"
 	echo "CRIANDO FOLDER src/components/App"
-	echo "---------------------------------"
+	echo "-------------------------------------------------------"
 	mkdir src/components/App
-	tmp="
-	import React from 'react';
-	import {Component} from 'react';
-	import {Fragment} from 'react';
-	import {createMuiTheme} from '@material-ui/core';
-	import {MuiThemeProvider} from '@material-ui/core';
-	import green from '@material-ui/core/colors/green';
-	import purple from '@material-ui/core/colors/purple';
-	import {Provider} from 'react-redux';
-	import {BrowserRouter} from 'react-router-dom';
-	import {Route} from 'react-router-dom';
-	import {Switch} from 'react-router-dom';
-	import 'typeface-roboto';
+	tmp="import React from 'react';
+import {Fragment} from 'react';
+import {createMuiTheme} from '@material-ui/core';
+import {MuiThemeProvider} from '@material-ui/core';
+import green from '@material-ui/core/colors/green';
+import purple from '@material-ui/core/colors/purple';
+import {BrowserRouter} from 'react-router-dom';
+import {Route} from 'react-router-dom';
+import {Switch} from 'react-router-dom';
+import 'typeface-roboto';
 
-	import './App.css';
-	import store from '../../store';
-	import routes from '../../routes';
-	import Header from '../Header';
+import './App.css';
+import routes from '../../routes';
+import Header from '../Header';
 
-	const theme = createMuiTheme({
-		 palette: {
-		     primary: green,
-		     secondary: purple,
-		 },
-		 status: {
-		     danger: 'orange',
-		 },
-		 typography: {
-		     useNextVariants: true,
-		 }
-	});
+const theme = createMuiTheme({
+	 palette: {
+	     primary: green,
+	     secondary: purple,
+	 },
+	 status: {
+	     danger: 'orange',
+	 },
+	 typography: {
+	     useNextVariants: true,
+	 }
+});
 
-	class App extends Component {
+const App = () => (
 
-		 render() {
+		<MuiThemeProvider theme={theme}>
+			<BrowserRouter>
+				<Fragment>
+					<Header/>
+					<Switch>
+						{
+							routes.map((route,index) => (
+								<Route
+									key={index}
+									path={route.path}
+									exact={route.exact}
+									component={route.component}
+								/>
+							))
+						}
+					</Switch>
+				</Fragment>
+			</BrowserRouter>
+		</MuiThemeProvider>
+);
 
-		     return (
-		         <Provider store={store}>
-
-		             <MuiThemeProvider theme={theme}>
-
-		                 <BrowserRouter>
-
-		                     <Fragment>
-
-		                         <Header/>
-
-		                         <Switch>
-		                             {
-		                                 routes.map((route,index) => (
-		                                     <Route
-		                                         key={index}
-		                                         path={route.path}
-		                                         exact={route.exact}
-		                                         component={route.component}
-		                                     />
-		                                 ))
-		                             }
-		                         </Switch>
-
-		                     </Fragment>
-
-		                 </BrowserRouter>
-
-		             </MuiThemeProvider>
-
-		         </Provider>
-		     );
-		 }
-	}
+export default App;
 	"
 	echo "$tmp" > src/components/App/index.js
 
-	tmp="
-	body
-	{
-		 margin: 0;
-		 padding: 0;
-		 font-family: 'Roboto', sans-serif;
-		 background-color: #E4E4E4;
-	}
-	"
+	tmp="body
+{
+	 margin: 0;
+	 padding: 0;
+	 font-family: 'Roboto', sans-serif;
+	 background-color: #E4E4E4;
+}"
 	echo "$tmp" > src/components/App/App.css
 
-	echo "---------------------------------"
+
+	echo "-------------------------------------------------------"
 	echo "CRIANDO FOLDER src/components/Header"
-	echo "---------------------------------"
+	echo "-------------------------------------------------------"
 	mkdir src/components/Header
-	tmp="
-	import React from 'react';
-	import AppBar from '@material-ui/core/AppBar';
-	import Toolbar from '@material-ui/core/Toolbar';
-	import Typography from '@material-ui/core/Typography';
-	import IconButton from '@material-ui/core/IconButton';
-	import MenuIcon from '@material-ui/icons/Menu';
+	tmp="import React from 'react';
+import {Fragment} from 'react';
+import {useState} from 'react';
+import {withRouter} from 'react-router';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Drawer from '@material-ui/core/Drawer';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
 
-	const Header = () => (
-		 <AppBar position='static' color='primary'>
-		     <Toolbar>
-		         <IconButton className='app-menu' color='inherit' aria-label='Menu'>
-		             <MenuIcon />
-		         </IconButton>
-		         <Typography variant='h6'>
-		             Agenda
-		         </Typography>
-		     </Toolbar>
-		 </AppBar>
+
+const Header = props => {
+
+	const [open, setOpen] = useState(false);
+
+	const handleDrawerOpen = () => setOpen(true);
+	const handleDrawerClose = () => setOpen(false);
+
+	const irPara = url => {
+		handleDrawerClose();
+		props.history.push( url );
+	}
+
+	return (
+		<Fragment>
+
+			<AppBar position='static' color='primary'>
+				<Toolbar>
+					<IconButton 
+						className='app-menu' 
+						color='inherit' 
+						aria-label='Menu'
+						onClick={handleDrawerOpen}
+					>
+						<MenuIcon />
+					</IconButton>
+					<Typography variant='h6' noWrap>
+						Exemplo
+					</Typography>
+				</Toolbar>
+			</AppBar>
+
+			<Drawer
+				variant='persistent'
+				anchor='left'
+				open={open}
+			>
+				<IconButton onClick={handleDrawerClose}><ChevronLeftIcon/></IconButton>
+
+				<Divider />
+
+				<ListItem 
+					button
+					onClick={() => alert('Item1')}
+				>
+              		<ListItemIcon><InboxIcon /></ListItemIcon>
+              		<ListItemText primary='Item 1' />
+				</ListItem>
+				
+				<ListItem 
+					button
+					onClick={() => irPara('/sobre')}
+				>
+					<ListItemIcon><InboxIcon /></ListItemIcon>
+					<ListItemText primary='Sobre' />
+		  		</ListItem>				
+
+			</Drawer>
+
+		</Fragment>			
 	);
+};
 
-	export default Header;
-	"
+export default withRouter(Header);"
 	echo "$tmp" > src/components/Header/index.js
 
-	echo "---------------------------------"
+
+	echo "-------------------------------------------------------"
 	echo "CRIANDO FOLDER src/components/Home"
-	echo "---------------------------------"
+	echo "-------------------------------------------------------"
 	mkdir src/components/Home
-	tmp="
-	import React from 'react';
+	tmp="import React from 'react';
 
-	const Home = () => (<h1>HOme</h1>);
+const Home = () => (<h1>HOme</h1>);
 
-	export default Home;
-	"
+export default Home;"
 	echo "$tmp" > src/components/Home/index.js
-	
 
-	echo "---------------------------------"
+
+	echo "-------------------------------------------------------"
 	echo "CRIANDO FOLDER src/components/Sobre"
-	echo "---------------------------------"
+	echo "-------------------------------------------------------"
 	mkdir src/components/Sobre
-	tmp="
-	import React from 'react';
+	tmp="import React from 'react';
 
-	const Sobre = () => (<h1>App exemplo</h1>);
+const Sobre = () => (<h1>App exemplo</h1>);
 
-	export default Sobre;
-	"
+export default Sobre;"
 	echo "$tmp" > src/components/Sobre/index.js
 
 
-	echo "---------------------------------"
+	echo "-------------------------------------------------------"
 	echo "CRIANDO FOLDER src/routes"
-	echo "---------------------------------"
+	echo "-------------------------------------------------------"
 	mkdir src/routes
-	tmp="
-	import React from 'react';
+	tmp="import React from 'react';
 
-	import Home from '../components/Home'
-	import Sobre from '../components/Sobre';
+import Home from '../components/Home'
+import Sobre from '../components/Sobre';
 
-	const routes = [
+const routes = [
+	 {
+	     path: '/',
+	     exact: true,
+	     component: () => ( <Home/> )
+	 },
 
-		 {
-		     path: '/',
-		     exact: true,
-		     component: () => ( <Home/> )
-		 },
+	 {
+	     path: '/sobre',
+	     exact: true,
+	     component: () => ( <Sobre/> )
+	 },
+];
 
-		 {
-		     path: '/sobre',
-		     exact: true,
-		     component: () => ( <Sobre/> )
-		 },
-	];
-
-	export default routes;
-	"
+export default routes;"
 	echo "$tmp" > src/routes/index.js
-
-	echo "---------------------------------"
-	echo "CRIANDO FOLDER src/store"
-	echo "---------------------------------"
-	mkdir src/store
-	mkdir src/store/actions
-	mkdir src/store/reducers
-	mkdir src/store/sagas
-
-	tmp="
-	import {createStore} from 'redux';
-
-	import reducers from './reducers';
-
-	const store = createStore(
-		reducers,
-		window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-	);
-
-	export default store;
-	EOL
-
-	cat > src/store/reducers/index.js <<EOL
-	import {combineReducers} from 'redux';
-
-	export default combineReducers({
-		root: () => ({})
-	});
-	"
-	echo "$tmp" > src/store/index.js
 
 }
 
+
+#
+# function: criar_folder_commons()
+#
+criar_folder_commons()
+{
+	echo "-------------------------------------------------------"
+	echo "CRIANDO FOLDER src/commons"
+	echo "-------------------------------------------------------"
+	mkdir src/components/commons
+	
+    mkdir src/components/commons/hoc
+	mkdir src/components/commons/hoc/comCustomBtnIcon
+	tmp="
+import IconButton from '@material-ui/core/IconButton';
+
+const comCustomBtnIcon = ComponenteBase => ({
+    children,
+    onClick
+}) => (
+    <IconButton
+        onClick={onClick}
+        color='inherit'>
+        <ComponenteBase>
+            {children}
+        </ComponenteBase>
+    </IconButton>
+);
+
+export default comCustomBtnIcon;"
+	echo "$tmp" > src/components/commons/hoc/comCustomBtnIcon/index.js
+
+	mkdir src/components/commons/icons;	
+
+	array=(Cancel Menu Search)
+	for i in ${array[@]}
+	do
+
+		folder=src/components/commons/icons/"CustomIcon${i}"
+		mkdir ${folder}
+		tmp="import React from 'react';
+import Icone from '@material-ui/icons/${i}';
+
+export default () => <Icone/>;"
+		echo "$tmp" > ${folder}/index.js
+
+		folder=src/components/commons/"CustomBtnIcon${i}"
+		mkdir ${folder}
+		tmp="import React from 'react';
+
+import comCustomBtnIcon from '../hoc/comCustomBtnIcon';
+import Icone from '../icons/Custom${i}';
+
+const Btn = () => <Icone/>;
+
+export default comCustomBtnIcon(Btn);"
+		echo "$tmp" > ${folder}/index.js
+
+	done
+}
+
+
+#
+# function: init()
+#
 init()
 {
 
@@ -410,6 +486,7 @@ init()
 	criar_arquivo_babelrc
 	config_webpack
 	criar_folder_src
+	criar_folder_commons
 
 	echo "---------------------------------------"
 	echo "FIM - Projeto configurado com WebPack 4"
